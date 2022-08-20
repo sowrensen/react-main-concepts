3ac73cd2cb6f14be32b74654a43e79011b0286dd
Sowren Sen <sowren@coveredpress.com>
2022-08-20 14:53:58 +0600

    component composition and mixing functional and class compoents

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const user = {
-  firstName: 'Sowren',
-  lastName: 'Sen',
-  age: 33,
-  website: 'https://sowrensen.dev'
-}
+const users = [
+  {
+    firstName: 'Sowren',
+    lastName: 'Sen',
+    age: 33,
+    website: 'https://sowrensen.dev'
+  },
+  {
+    firstName: 'Walter',
+    lastName: 'White',
+    age: 54,
+    website: 'https://walter-white.meth'
+  },
+  {
+    firstName: 'Jessy',
+    lastName: 'Pinkman',
+    age: 29,
+    website: 'https://jessy-yo.bitch'
+  }
+]
 
 function formatName(firstName, lastName) {
   return `${firstName} ${lastName}`;
@@ -19,10 +33,21 @@ class UserInfo extends React.Component {
         <div>User: {formatName(this.props.user.firstName, this.props.user.lastName)}</div>
         <div>Age: {this.props.user.age}</div>
         <div>Website: <a href={this.props.user.website} target='_blank'>Personal website</a></div>
+        <br></br>
       </div>
     )
   }
 }
 
+function App() {
+  return (
+    <div>
+      <UserInfo user={users[0]}/>
+      <UserInfo user={users[1]}/>
+      <UserInfo user={users[2]}/>
+    </div>
+  )
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<UserInfo user={user} />)
+root.render(<App/>)
