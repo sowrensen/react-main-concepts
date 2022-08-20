b00234575a5b9db19843e44af1e136fa5e48265b
Sowren Sen <sowren@coveredpress.com>
2022-08-20 22:24:37 +0600

    state and lifecycle methods

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client';
 
 const users = [
   {
-    firstName: 'Sowren',
-    lastName: 'Sen',
-    age: 33,
-    website: 'https://sowrensen.dev'
+    firstName: 'Saul',
+    lastName: 'Goodman',
+    age: 48,
+    website: 'https://bettercallsaul.com'
   },
   {
     firstName: 'Walter',
@@ -39,12 +39,50 @@ class UserInfo extends React.Component {
   }
 }
 
+class Clock extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { date: new Date() }
+  }
+
+  componentDidMount() {
+    this.timerId = setInterval(() => {
+      this.tick()
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId)
+  }
+
+  tick() {
+    this.setState((prevState, props) => ({ date: new Date() }))
+    // We can do this like following:
+    // this.setState({date: new Date()})
+    // But there's a catch due to the asynchronous nature of the state,
+    // so if something like this.setState({counter: this.state.counter + this.increment})
+    // is written, it might result into error. In that case use an arrow function like we used.
+  }
+
+  render() {
+    return (
+      <div>
+        <p><strong>Most Wanted</strong></p>
+        <p>System Time: {this.state.date.toLocaleTimeString()}</p>
+      </div>
+    )
+  }
+}
+
 function App() {
   return (
     <div>
-      {users.map((user, index) => {
-        return <UserInfo user={user} key={index} />
-      })}
+      <Clock />
+      <div>
+        {users.map((user, index) => {
+          return <UserInfo user={user} key={index} />
+        })}
+      </div>
     </div>
   )
 }
